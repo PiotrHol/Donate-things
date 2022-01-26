12116ccc6427ada900ca034c1659216eebf4660a
Piotr Hołub <piotrekholub@wp.pl>
2022-01-26 13:41:46 +0100

    Added routing to App component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
-function App() {
+import { HashRouter, Switch, Route, Redirect } from "react-router-dom";
+import { PrivateRouter } from "./PrivateRouter";
+import { Home } from "./components/Home/Home";
+
+export const App = () => {
   return (
-    <div>
-      <h1>Donate-things</h1>
-    </div>
+    <HashRouter basename="/">
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/logowanie">
+          Logowanie
+        </Route>
+        <Route path="/rejestracja">
+          Rejestracja
+        </Route>
+        <Route path="/wylogowano">
+          Wylogowano
+        </Route>
+        <PrivateRouter path="/oddaj-rzeczy">
+          Formularz
+        </PrivateRouter>
+        <Route path="*">
+          <Redirect to="/" />
+        </Route>
+      </Switch>
+    </HashRouter>
   );
 }
-
-export default App;
