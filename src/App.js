99d728844913093a7ce640f7b85f4533a431ea2a
Piotr Hołub <piotrekholub@wp.pl>
2022-01-28 13:44:13 +0100

    Added main block to App component

--- src/App.js
+++ src/App.js
@@ -1,30 +1,33 @@
 import { HashRouter, Switch, Route, Redirect } from "react-router-dom";
 import { PrivateRouter } from "./PrivateRouter";
 import { Home } from "./components/Home/Home";
+import "./scss/main.scss";
 
 export const App = () => {
   return (
     <HashRouter basename="/">
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route path="/logowanie">
-          Logowanie
-        </Route>
-        <Route path="/rejestracja">
-          Rejestracja
-        </Route>
-        <Route path="/wylogowano">
-          Wylogowano
-        </Route>
-        <PrivateRouter path="/oddaj-rzeczy">
-          Formularz
-        </PrivateRouter>
-        <Route path="*">
-          <Redirect to="/" />
-        </Route>
-      </Switch>
+      <div className="app">
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/logowanie">
+            Logowanie
+          </Route>
+          <Route path="/rejestracja">
+            Rejestracja
+          </Route>
+          <Route path="/wylogowano">
+            Wylogowano
+          </Route>
+          <PrivateRouter path="/oddaj-rzeczy">
+            Formularz
+          </PrivateRouter>
+          <Route path="*">
+            <Redirect to="/" />
+          </Route>
+        </Switch>
+      </div>
     </HashRouter>
   );
 }
