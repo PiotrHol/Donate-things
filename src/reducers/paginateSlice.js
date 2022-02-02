8d78c832e49087f46112eebf8fbfa5cb32c0137b
Piotr Hołub <piotrekholub@wp.pl>
2022-02-02 12:47:57 +0100

    Added more variables to state and more paginateReducer options

--- src/reducers/paginateSlice.js
+++ src/reducers/paginateSlice.js
@@ -1,57 +1,125 @@
 import { getFirestore, getDocs, collection } from "firebase/firestore";
 
 const initialState = {
-    fundations: [],
-    organizations: [],
-    collections: []
+    pagination: {
+        fundations: [],
+        organizations: [],
+        collections: [],
+        descriptions: {
+          fundations:
+            "W naszej bazie znajdziesz listę zweryfikowanych Fundacji, z którymi współpracujemy. Możesz sprawdzić czym się zajmują, komu pomagają i czego potrzebują.",
+          organizations:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.",
+          collections:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.",
+        },
+        currentList: null,
+        currentNavElement: "fundations",
+        currentDescription: null,
+        currentPage: 1,
+        itemsPerPage: 3
+    }
 };
 
 const fetchData = async (dispatch) => {
-    const dataBase = getFirestore();
-    
-    const fundationsFromBase = await getDocs(collection(dataBase, "fundations"));
-    const fundations = [];
-    fundationsFromBase.forEach(fundation => fundations.push({
-        id: fundation.id,
-        ...fundation.data()
-    }));
+  const dataBase = getFirestore();
 
-    const organizationsFromBase = await getDocs(collection(dataBase, "organizations"));
-    const organizations = [];
-    organizationsFromBase.forEach(organization => organizations.push({
-        id: organization.id,
-        ...organization.data()
-    }));
+  const fundationsFromBase = await getDocs(collection(dataBase, "fundations"));
+  const fundations = [];
+  fundationsFromBase.forEach((fundation) =>
+    fundations.push({
+      id: fundation.id,
+      ...fundation.data(),
+    })
+  );
 
-    const collectionsFromBase = await getDocs(collection(dataBase, "collections"));
-    const collections = [];
-    collectionsFromBase.forEach(collection => collections.push({
-        id: collection.id,
-        ...collection.data()
-    }));
+  const organizationsFromBase = await getDocs(
+    collection(dataBase, "organizations")
+  );
+  const organizations = [];
+  organizationsFromBase.forEach((organization) =>
+    organizations.push({
+      id: organization.id,
+      ...organization.data(),
+    })
+  );
 
-    dispatch({type: "fundations/fetch", payload: {
-        fundations,
-        organizations,
-        collections
-    }});
-}
+  const collectionsFromBase = await getDocs(
+    collection(dataBase, "collections")
+  );
+  const collections = [];
+  collectionsFromBase.forEach((collection) =>
+    collections.push({
+      id: collection.id,
+      ...collection.data(),
+    })
+  );
 
-const paginateReducer = (state = initialState, {type, payload}) => {
-    switch(type) {
-        case "fundations/fetch":
-            return {
-                ...state,
-                fundations: payload.fundations,
-                organizations: payload.organizations,
-                collections: payload.collections
-            };
-        default:
-            return state;
-    }
-}
+  dispatch({
+    type: "pagination/fetch",
+    payload: {
+      fundations,
+      organizations,
+      collections,
+    },
+  });
+};
 
-export {
-    paginateReducer,
-    fetchData
-}
+const paginateReducer = (state = initialState, { type, payload }) => {
+  switch (type) {
+    case "pagination/fetch":
+      return {
+        ...state,
+        pagination: {
+            ...state.pagination,
+            fundations: payload.fundations,
+            organizations: payload.organizations,
+            collections: payload.collections,
+            currentList: payload.fundations,
+            currentDescription: state.pagination.descriptions.fundations
+        }
+      };
+    case "pagination/setCurrentPage":
+        return {
+            ...state,
+            pagination: {
+                ...state.pagination,
+                currentPage: payload
+            }
+        }
+    case "pagination/setListAsOrg":
+        return {
+            ...state,
+            pagination: {
+                ...state.pagination,
+                currentList: state.pagination.organizations,
+                currentDescription: state.pagination.descriptions.organizations,
+                currentNavElement: "organizations"
+            }
+        }
+    case "pagination/setListAsFun":
+        return {
+            ...state,
+            pagination: {
+                ...state.pagination,
+                currentList: state.pagination.fundations,
+                currentDescription: state.pagination.descriptions.fundations,
+                currentNavElement: "fundations"
+            }
+        }
+    case "pagination/setListAsColl":
+        return {
+            ...state,
+            pagination: {
+                ...state.pagination,
+                currentList: state.pagination.collections,
+                currentDescription: state.pagination.descriptions.collections,
+                currentNavElement: "collections"
+            }
+        }
+    default:
+      return state;
+  }
+};
+
+export { paginateReducer, fetchData };
