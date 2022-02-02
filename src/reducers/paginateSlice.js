dc2ddc1f7482024c38926aeeff0513b409d1adb1
Piotr Hołub <piotrekholub@wp.pl>
2022-02-02 01:38:57 +0100

    Added fetchData function to paginateReducer

--- src/reducers/paginateSlice.js
+++ src/reducers/paginateSlice.js
@@ -6,6 +6,37 @@ const initialState = {
     collections: []
 };
 
+const fetchData = async (dispatch) => {
+    const dataBase = getFirestore();
+    
+    const fundationsFromBase = await getDocs(collection(dataBase, "fundations"));
+    const fundations = [];
+    fundationsFromBase.forEach(fundation => fundations.push({
+        id: fundation.id,
+        ...fundation.data()
+    }));
+
+    const organizationsFromBase = await getDocs(collection(dataBase, "organizations"));
+    const organizations = [];
+    organizationsFromBase.forEach(organization => organizations.push({
+        id: organization.id,
+        ...organization.data()
+    }));
+
+    const collectionsFromBase = await getDocs(collection(dataBase, "collections"));
+    const collections = [];
+    collectionsFromBase.forEach(collection => collections.push({
+        id: collection.id,
+        ...collection.data()
+    }));
+
+    dispatch({type: "fundations/fetch", payload: {
+        fundations,
+        organizations,
+        collections
+    }});
+}
+
 const paginateReducer = (state = initialState, {type, payload}) => {
     switch(type) {
         case "fundations/fetch":
@@ -21,5 +52,6 @@ const paginateReducer = (state = initialState, {type, payload}) => {
 }
 
 export {
-    paginateReducer
+    paginateReducer,
+    fetchData
 }
