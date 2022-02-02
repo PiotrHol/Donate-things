7c3a39ff32e5014963d4f625577657aa4653ee1a
Piotr Hołub <piotrekholub@wp.pl>
2022-02-02 01:39:35 +0100

    Added redux store to app

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { App } from './App';
-import './scss/main.scss';
+import React from "react";
+import ReactDOM from "react-dom";
+import { App } from "./App";
+import "./scss/main.scss";
 // eslint-disable-next-line
 import app from "./firebase";
+import { Provider } from "react-redux";
+import store from "./store";
+import { fetchData } from "./reducers/paginateSlice";
+
+store.dispatch(fetchData);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
