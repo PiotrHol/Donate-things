801b12424e4808895c3d2b9d54da7a7f3b971d2b
Piotr Hołub <piotrekholub@wp.pl>
2022-02-06 13:48:05 +0100

    Created Login component and added it to App component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { HashRouter, Switch, Route, Redirect } from "react-router-dom";
 import { PrivateRouter } from "./PrivateRouter";
 import { Home } from "./components/Home/Home";
 import "./scss/main.scss";
+import { Login } from "./components/Login/Login";
 
 export const App = () => {
   return (
@@ -12,7 +13,7 @@ export const App = () => {
             <Home />
           </Route>
           <Route path="/logowanie">
-            Logowanie
+            <Login />
           </Route>
           <Route path="/rejestracja">
             Rejestracja
