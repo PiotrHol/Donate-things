ad7f1cab85b38f76b69de1013c0c0548e9ce8d6a
Piotr Hołub <piotrekholub@wp.pl>
2022-02-08 11:25:45 +0100

    Updated redux store

--- src/store.js
+++ src/store.js
@@ -1,8 +1,14 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, combineReducers } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import thunkMiddleware from "redux-thunk";
 import { paginateReducer } from "./reducers/paginateSlice";
+import { authReducer } from "./reducers/authSlice";
 
-const store = createStore(paginateReducer, composeWithDevTools(applyMiddleware(thunkMiddleware)));
+const rootReducer = combineReducers({
+    auth: authReducer,
+    pagination: paginateReducer
+})
+
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunkMiddleware)));
 
 export default store;
