a9d4eec9848e5c2cd50ddfe6c1b543fda1357e9d
Piotr Hołub <piotrekholub@wp.pl>
2022-02-08 11:23:09 +0100

    Updated paginateReducer

--- src/reducers/paginateSlice.js
+++ src/reducers/paginateSlice.js
@@ -2,24 +2,22 @@ import { getFirestore, getDocs, collection } from "firebase/firestore";
 import { actionsType, getData } from "../actions/paginateActions";
 
 const initialState = {
-  pagination: {
-    fundations: [],
-    organizations: [],
-    collections: [],
-    descriptions: {
-      fundations:
-        "W naszej bazie znajdziesz listę zweryfikowanych Fundacji, z którymi współpracujemy. Możesz sprawdzić czym się zajmują, komu pomagają i czego potrzebują.",
-      organizations:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.",
-      collections:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.",
-    },
-    currentList: null,
-    currentNavElement: "fundations",
-    currentDescription: null,
-    currentPage: 1,
-    itemsPerPage: 3,
+  fundations: [],
+  organizations: [],
+  collections: [],
+  descriptions: {
+    fundations:
+      "W naszej bazie znajdziesz listę zweryfikowanych Fundacji, z którymi współpracujemy. Możesz sprawdzić czym się zajmują, komu pomagają i czego potrzebują.",
+    organizations:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.",
+    collections:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.",
   },
+  currentList: null,
+  currentNavElement: "fundations",
+  currentDescription: null,
+  currentPage: 1,
+  itemsPerPage: 3,
 };
 
 const fetchData = async (dispatch) => {
@@ -70,55 +68,40 @@ const paginateReducer = (state = initialState, { type, payload }) => {
     case actionsType.getPaginateData:
       return {
         ...state,
-        pagination: {
-          ...state.pagination,
-          fundations: payload.fundations,
-          organizations: payload.organizations,
-          collections: payload.collections,
-          currentList: payload.fundations,
-          currentDescription: state.pagination.descriptions.fundations,
-        },
+        fundations: payload.fundations,
+        organizations: payload.organizations,
+        collections: payload.collections,
+        currentList: payload.fundations,
+        currentDescription: state.descriptions.fundations,
       };
     case actionsType.setPaginatePage:
       return {
         ...state,
-        pagination: {
-          ...state.pagination,
-          currentPage: payload,
-        },
+        currentPage: payload,
       };
     case actionsType.setOrgList:
       return {
         ...state,
-        pagination: {
-          ...state.pagination,
-          currentList: state.pagination.organizations,
-          currentDescription: state.pagination.descriptions.organizations,
-          currentNavElement: "organizations",
-          currentPage: 1,
-        },
+        currentList: state.organizations,
+        currentDescription: state.descriptions.organizations,
+        currentNavElement: "organizations",
+        currentPage: 1,
       };
     case actionsType.setFundList:
       return {
         ...state,
-        pagination: {
-          ...state.pagination,
-          currentList: state.pagination.fundations,
-          currentDescription: state.pagination.descriptions.fundations,
-          currentNavElement: "fundations",
-          currentPage: 1,
-        },
+        currentList: state.fundations,
+        currentDescription: state.descriptions.fundations,
+        currentNavElement: "fundations",
+        currentPage: 1,
       };
     case actionsType.setCollList:
       return {
         ...state,
-        pagination: {
-          ...state.pagination,
-          currentList: state.pagination.collections,
-          currentDescription: state.pagination.descriptions.collections,
-          currentNavElement: "collections",
-          currentPage: 1,
-        },
+        currentList: state.collections,
+        currentDescription: state.descriptions.collections,
+        currentNavElement: "collections",
+        currentPage: 1,
       };
     default:
       return state;
