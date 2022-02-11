f0950383e10985bfa3e7396c36e3d73f539d2d79
Piotr Hołub <piotrekholub@wp.pl>
2022-02-11 12:31:42 +0100

    Added FormContent component to FormPage component

--- src/components/FormPage/FormPage.js
+++ src/components/FormPage/FormPage.js
@@ -2,11 +2,13 @@ import React from "react";
 import "./formPage.scss";
 import { FormHeader } from "../FormHeader/FormHeader";
 import { Contact } from "../Contact/Contact";
+import { FormContent } from "../FormContent/FormContent";
 
 export const FormPage = () => {
     return (
         <div className="form-page">
             <FormHeader />
+            <FormContent />
             <Contact />
         </div>
     );
