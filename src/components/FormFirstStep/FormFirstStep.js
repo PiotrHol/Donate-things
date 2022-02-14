c214fca3ab9483c6b70a802ff6a6e8d5ec4c0bd6
Piotr Hołub <piotrekholub@wp.pl>
2022-02-14 11:10:47 +0100

    Added validation to FormFirstStep component

--- src/components/FormFirstStep/FormFirstStep.js
+++ src/components/FormFirstStep/FormFirstStep.js
@@ -1,8 +1,18 @@
 import React from "react";
 import "../FormContent/formContent.scss";
 import "./formFirstStep.scss";
+import { useForm } from "react-hook-form";
 
 export const FormFirstStep = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data.things);
+  };
   return (
     <div className="form-content">
       <div className="form-content__important">
@@ -13,39 +23,65 @@ export const FormFirstStep = () => {
         </p>
       </div>
       <div className="form-content__main">
-        <form className="form-content__form">
+        <form className="form-content__form" onSubmit={handleSubmit(onSubmit)}>
           <h3 className="form-content__form-step">Krok 1/4</h3>
           <h2 className="form-content__form-title">Zaznacz co chcesz oddać:</h2>
           <div className="form-first-step">
             <label className="form-first-step__label">
-              <input className="form-first-step__input" type="radio" />
+              <input
+                {...register("things", { required: true })}
+                className="form-first-step__input"
+                value="ubrania, które nadają się do ponownego użycia"
+                type="radio"
+              />
               <span className="form-first-step__span" />
               <p className="form-first-step__label-text">
                 ubrania, które nadają się do ponownego użycia
               </p>
             </label>
             <label className="form-first-step__label">
-              <input className="form-first-step__input" type="radio" />
+              <input
+                {...register("things", { required: true })}
+                className="form-first-step__input"
+                value="ubrania, do wyrzucenia"
+                type="radio"
+              />
               <span className="form-first-step__span" />
               <p className="form-first-step__label-text">
                 ubrania, do wyrzucenia
               </p>
             </label>
             <label className="form-first-step__label">
-              <input className="form-first-step__input" type="radio" />
+              <input
+                {...register("things", { required: true })}
+                className="form-first-step__input"
+                value="zabawki"
+                type="radio"
+              />
               <span className="form-first-step__span" />
               <p className="form-first-step__label-text">zabawki</p>
             </label>
             <label className="form-first-step__label">
-              <input className="form-first-step__input" type="radio" />
+              <input
+                {...register("things", { required: true })}
+                className="form-first-step__input"
+                value="książki"
+                type="radio"
+              />
               <span className="form-first-step__span" />
               <p className="form-first-step__label-text">książki</p>
             </label>
             <label className="form-first-step__label">
-              <input className="form-first-step__input" type="radio" />
+              <input
+                {...register("things", { required: true })}
+                className="form-first-step__input"
+                value="Inne"
+                type="radio"
+              />
               <span className="form-first-step__span" />
               <p className="form-first-step__label-text">Inne</p>
             </label>
+            {errors.things && <p className="form-content__form-error">Wybierz co chcesz oddać!</p>}
             <button className="form-content__form-btn" type="submit">
               Dalej
             </button>
