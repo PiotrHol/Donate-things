66dd3c8c839270b66af012a3796cd9840af06ff2
Piotr Hołub <piotrekholub@wp.pl>
2022-03-01 21:55:17 +0100

    Added scrolling to top after loading in FormPage component

--- src/components/FormPage/FormPage.js
+++ src/components/FormPage/FormPage.js
@@ -1,15 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./formPage.scss";
 import { FormHeader } from "../FormHeader/FormHeader";
 import { Contact } from "../Contact/Contact";
 import { FormContent } from "../FormContent/FormContent";
+import { animateScroll } from "react-scroll";
 
 export const FormPage = () => {
-    return (
-        <div className="form-page">
-            <FormHeader />
-            <FormContent />
-            <Contact />
-        </div>
-    );
-}
+  useEffect(() => {
+    animateScroll.scrollToTop();
+  }, []);
+
+  return (
+    <div className="form-page">
+      <FormHeader />
+      <FormContent />
+      <Contact />
+    </div>
+  );
+};
