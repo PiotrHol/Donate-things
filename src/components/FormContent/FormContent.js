d334603ac66648148dadfe19688c8a3e88011a90
Piotr Hołub <piotrekholub@wp.pl>
2022-04-01 01:29:26 +0200

    Added ThankYou component to FormContent component

--- src/components/FormContent/FormContent.js
+++ src/components/FormContent/FormContent.js
@@ -6,6 +6,7 @@ import { FormSecondStep } from "../FormSecondStep/FormSecondStep";
 import { FormThirdStep } from "../FormThirdStep/FormThirdStep";
 import { FormFourthStep } from "../FormFourthStep/FormFourthStep";
 import { Summary } from "../Summary/Summary";
+import { ThankYou } from "../ThankYou/ThankYou";
 
 export const FormContent = () => {
   const currentStep = useSelector((state) => state.form.currentStep);
@@ -21,6 +22,8 @@ export const FormContent = () => {
       return <FormFourthStep />;
     case 5:
       return <Summary />;
+    case 6:
+      return <ThankYou />;
     default:
       return <FormFirstStep />;
   }
